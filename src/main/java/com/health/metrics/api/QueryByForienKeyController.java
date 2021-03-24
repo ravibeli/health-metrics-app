2d307b64c122d4496962b4bbf3b885ad00c07143
ravibeli@gmail.com <ravibeli@gmail.com>
2021-03-24 18:39:50 +0530

    Added controller to perform query by foreign key

---
+++ src/main/java/com/health/metrics/api/QueryByForienKeyController.java
@@ -0,0 +1,42 @@
+package com.health.metrics.api;
+
+import com.health.metrics.dto.AggregationUserHealthParamsDTO;
+import com.health.metrics.dto.UserHealthParamsDTO;
+import com.health.metrics.entity.UserHealthParams;
+import com.health.metrics.mapper.UserHealthParamsMapper;
+import com.health.metrics.repository.UserHealthParamsRepository;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ravibeli
+ * @project health-metrics-app
+ * @created on 19 Mar, 2021 12:05 AM
+ **/
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/userhealth")
+public class QueryByForienKeyController {
+
+    @Autowired
+    UserHealthParamsRepository userHealthParamsRepository;
+
+    @GetMapping(value = "/user/{userId}/daily")
+    public ResponseEntity<List<UserHealthParamsDTO>> getHealthMetricsDailyReport(@PathVariable(value = "userId", required = true) Long userId)
+        throws Exception {
+        List<UserHealthParams> userHealthParams = userHealthParamsRepository.findByUserId(userId);
+        log.info("userHealthParams = {}", userHealthParams);
+        List<UserHealthParamsDTO> userHealthParamsDTOS = Mappers.getMapper(UserHealthParamsMapper.class)
+                                                            .toUserHealthParamsDTOList(userHealthParams);
+        return new ResponseEntity<>(userHealthParamsDTOS, HttpStatus.OK);
+    }
+}
